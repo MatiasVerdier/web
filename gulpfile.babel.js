6807b2f9063e73b6adbc8030f90b2a625717d69a
Verdier, Matias <verdier.matias@gmail.com>
2017-03-24 02:02:47 -0300

    hugo version as environment variable

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ import replace from "gulp-replace";
 import cssnano from "cssnano";
 
 const browserSync = BrowserSync.create();
-const hugoBin = "hugo";
+const hugoBin = process.env.HUGO_VERSION ? process.env.HUGO_VERSION : "hugo";
 const defaultArgs = ["-d", "../dist", "-s", "site"];
 
 gulp.task("hugo", (cb) => buildSite(cb));
